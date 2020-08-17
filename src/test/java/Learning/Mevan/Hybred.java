32c177d73b6d09c6d5a6f4132f972b7156bcf807
mac <macdublin@yahoo.com>
2020-08-18 01:29:30 +0700

    code added on 18th Aug at 1.30am

---
+++ src/test/java/Learning/Mevan/Hybred.java
@@ -0,0 +1,16 @@
+package Learning.Mevan;
+
+import org.testng.annotations.Test;
+
+public class Hybred {
+
+	@Test
+	public void demo7(){
+		System.out.println("demo7");
+	}
+	@Test
+	public void demo8(){
+		System.out.println("demo8");
+	}
+	
+}
